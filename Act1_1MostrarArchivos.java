456d037916feab8d0ca34bfcb9f1bc6b701d2fe1
Carlos Aguirre Vozmediano <38355125+CodigoBase2018@users.noreply.github.com>
2018-10-03 13:19:05 +0200

    Add files via upload

--- Act1_1MostrarArchivos.java
+++ Act1_1MostrarArchivos.java
@@ -5,42 +5,47 @@ import java.util.ArrayList;
 
 public class Act1_1MostrarArchivos {
     private ArrayList<String> listaArchivos;
+    private File directorioActual;
     
     public Act1_1MostrarArchivos(String[] listaDirectorios){
         for(String directorio : listaDirectorios){
-            
             this.listaArchivos = new ArrayList();
-            System.out.println("La ruta actual es: " + directorio);
+            this.directorioActual = new File(directorio);
 
-            File ruta = new File(directorio);
-            
-            if(this.getListaArchivos(ruta)){
-                System.out.println("Se encontraron " + Integer.toString(this.listaArchivos.size()) + " archivos:");
-                for(String nombreArchivo : this.listaArchivos){System.out.println("- " + nombreArchivo);}
-            }
-            else{System.out.println("Esta ruta no es valida por lo que no se puede comprobar.");}
-            
-            System.out.println("");
+            this.getListaArchivos();
         }
     }
     
-    // Obtiene la lista de archivos si la ruta actual es correcta
-    // Devuelve true si es correcta por lo que se ha creado el listado y false en caso contrario
-    private boolean getListaArchivos(File rutaActual){
-        if(rutaActual.exists()){
-            String[] listaElementos = rutaActual.list();
+    // Obtiene la lista de archivos si la ruta actual es correcta:
+    private void getListaArchivos(){
+        if(this.directorioActual.exists()){
+            String[] listaElementos = this.directorioActual.list();
             
             for(int i = 0; i < listaElementos.length; i++){ 
-                File elemento = new File(rutaActual, listaElementos[i]);
+                File elemento = new File(this.directorioActual, listaElementos[i]);
                 if(elemento.isFile()){this.listaArchivos.add(listaElementos[i]);}
             }
-            return true;
+            
+            this.mostrarArchivos();
         }
-        else{return false;}
+        else{System.out.println("La ruta: " + this.directorioActual + "\nNo es correcta.");}
+    }
+    
+    // Muestra la lista de archivos:
+    private void mostrarArchivos(){
+        System.out.println("La ruta actual es: " + this.directorioActual);
+        
+        if(this.listaArchivos.size() > 0){
+            System.out.println("Se encontraron " + Integer.toString(this.listaArchivos.size()) + " archivos:");
+            for(String nombreArchivo : this.listaArchivos){System.out.println("- " + nombreArchivo);}
+        }
+        else{System.out.println("No hay archivos que mostrar.");}
+
+        System.out.println("");
     }
     
     public static void main(String[] args) {
         
-        new Act1_1MostrarArchivos(args); // 1) Creamos un objeto de la actividad 1_1 y le pasamos los argumentos 
+        new Act1_1MostrarArchivos(args); 
     }
 }
